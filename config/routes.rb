6e4cfcfa81c715c984b67bca1b6d4714ea5c4c98
surya lasmara <naborsmalillie@gmail.com>
2018-04-16 20:38:51 +0700

    Create the About page and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #get 'pages/home'# 
-  
+  #get 'pages/home'#
+
   root 'pages#home' # Redirect to index home
-  
-  
-  
-  
-  
-  
+
+  get 'about' => 'pages#about'
+
+
+
 end
